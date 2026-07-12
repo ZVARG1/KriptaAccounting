a3ac7e6be7c764e299cf9ae432d42b2e77b13fca
ZVARG1 <glistglistovich@gmail.com>
2026-07-12 18:38:41 +0900

    Update Application.cs

--- Assets/Scripts/Database/Tables/Application.cs
+++ Assets/Scripts/Database/Tables/Application.cs
@@ -1,13 +1,20 @@
 using SQLite;
 using System;
 
-public class Application {
-    [PrimaryKey, AutoIncrement] public int ApplicationID { get; set; }
+public class Application
+{
+    [PrimaryKey, AutoIncrement]
+    public int ApplicationID { get; set; }
+
     public int ClientID { get; set; }
+
     public int ServiceID { get; set; }
-    public DateTime DateCreated { get; set; }
-    public string Status { get; set; }
-    public string Priority { get; set; }
 
     public int EmployeeID { get; set; }
+
+    public DateTime DateCreated { get; set; }
+
+    public string Status { get; set; }
+
+    public string Priority { get; set; }
 }
